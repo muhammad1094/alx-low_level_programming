f488caf32c09effb4af5ec5851f7d1d22206137a
muhammad1094 <muhammadussamaa94@gmail.com>
2023-10-09 15:29:50 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: 'print form a to z in lower and upper case'
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+	char CH = 'A';
+
+	while (ch <= 'z');
+	{
+		putchar(ch);
+		ch++;
+	}
+
+	while (CH <= 'Z');
+	{
+		putchar(CH);
+		CH++;
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
