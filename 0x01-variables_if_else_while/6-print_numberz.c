a85e7e0f44ed53ccb974134affe88e10768a8127
muhammad1094 <muhammadussamaa94@gmail.com>
2023-10-09 18:05:17 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: A C program that prints with printf function
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int digit = 0
+		while (digit <= 9)
+	{
+		putchar(digit = '0');
+		digit++;
+	}
+	putchar('\n');
+	return (0);
+}
