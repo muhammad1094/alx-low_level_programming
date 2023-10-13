5cdd3af70c1672363977c521b9d345dff97b86fd
muhammad1094 <muhammadussamaa94@gmail.com>
2023-10-13 16:11:48 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints from 0 to 9
+ *
+ * Return: always 0 (Success)
+*/
+
+void print_numbers(void)
+{
+	int num = 0;
+
+	do {
+		_putchar(num + 48);
+		num++,
+	} while (num >= 0 && num <= 9)
+	_putchar('\n');
+}
