c842653b12d78ece938d9853722dd4254eb1a907
muhammad1094 <muhammadussamaa94@gmail.com>
2023-11-08 13:15:22 -0800

    3

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -36,7 +36,7 @@ int op_sub(int a, int b)
 
 int op_mul(int a, int b)
 {
-	return (a * b)
+	return (a * b);
 }
 
 
@@ -50,7 +50,7 @@ int op_mul(int a, int b)
 
 int op_div(int a, int b)
 {
-	return (a / b)
+	return (a / b);
 }
 
 /**
@@ -63,5 +63,5 @@ int op_div(int a, int b)
 
 int op_mod(int a, int b)
 {
-	return (a % b)
+	return (a % b);
 }
